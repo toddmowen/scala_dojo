270cda26695b93ac12f2907918630df70607c7eb
Todd Owen <todd.m.owen@gmail.com>
2012-04-09 12:05:31 +0800

    Solution to dojo problem.

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -3,9 +3,14 @@ package dojo
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
-    case _ => None
+    case Ping => Pong
+    case Pong => Ping
   }
 
-  def receive(msg: Any) = None
+  def receive(msg: Any) = msg match{
+    case pingpong: PingPongMessage => handleMsg(pingpong)
+    case "stop" => "stopped"
+    case ("echo", s: String) => s    // limiting type of s to String is not necessary, but let's be conservative
+  }
 
 }
